4cac834ad0c1e04f0096223ac959d846bba33172
react-learner <suhashsai1999@gmail.com>
2024-03-04 16:57:35 +0530

    day 01 completed

--- day-01/src/App.jsx
+++ day-01/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div class="main-container">
+      <div class="hundred-container">
+        <div class="hundred" >
+          <p>IOO</p>
+        </div>
+        <div class="days" >
+          <p >DAYS</p>
+        </div>
+        <div class="challenge" >
+          <p > CSS CHALLENGE</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
