d7bec6be0026de2e9dd05eb299290f1b6f825ec4
react-learner <suhashsai1999@gmail.com>
2024-05-08 12:12:39 +0530

    completed hero section

--- day-21/src/components/Hero.jsx
+++ day-21/src/components/Hero.jsx
@@ -1,12 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Hero.css";
 const Hero = () => {
   return (
     <div className=" mt-5 pt-4 padding-container">
-      <div className="row d-flex align-items-start ">
-        <h1 className=" col-11 text-start text-uppercase">
-          the birthplace of the next b2b software stars
-        </h1>
-        <h1 className=" mt-0 pt-0 col-1 hero-animation">*</h1>
+      <div className="container-fluid">
+        <div className="row d-flex align-items-start ">
+          <h1 className=" col-11 text-start text-uppercase">
+            the birthplace of the next b2b software stars
+          </h1>
+          <FontAwesomeIcon
+            icon={faAsterisk}
+            className="col-1 col-auto hero-animation"
+          />
+        </div>
+      </div>
+      <div className="container-fluid my-3">
+        <div className="row">
+          <div className="col-12 hero--background-video"></div>
+        </div>
       </div>
     </div>
   );
