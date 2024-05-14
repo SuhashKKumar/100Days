73b65cd75bceaa2972cecf2f60f59af4a422ad38
react-learner <suhashsai1999@gmail.com>
2024-05-14 16:53:16 +0530

    checking with heights

--- day-21/src/components/Allies.jsx
+++ day-21/src/components/Allies.jsx
@@ -6,7 +6,6 @@ const Allies = () => {
   return (
     <div className="  padding-container my-5 common-container">
       <div className="parallax-text "></div>
-
       <div className="row allies-container">
         <div className="d-flex flex-column uneven-position">
           {AlliesCardData.map(({ id, img, description }) => {
