823771d45fadb28b07c441ed2bf1d13900813d66
react-learner <suhashsai1999@gmail.com>
2024-05-17 15:07:43 +0530

    brilliant section responsiveness completed

--- day-21/src/components/Brilliant.jsx
+++ day-21/src/components/Brilliant.jsx
@@ -1,5 +1,6 @@
 import DescriptionText from "../misc/DescriptionText";
 import SecondaryHeadline from "../misc/SecondaryHeadline";
+import '../styles/Brilliant.css'
 const Brilliant = () => {
     const brilliantImages =[1,2,3]
   const description =
@@ -8,18 +9,18 @@ const Brilliant = () => {
     <div className="padding-container my-5 common-container">
       <div className="container-fluid py-5">
         <div className="row ">
-          <div className="col-12 col-lg-6">
+          <div className="col-12 col-xl-6 mb-lg-4 order-0">
             <SecondaryHeadline text="For the b2bbrilliant" />
           </div>
-          <div className="col-12 col-lg-6">
+          <div className="col-12 col-xl-6 order-1">
             <DescriptionText text={description} />
           </div>
         </div>
       </div>
       <div className="container-fluid">
-        <div className="row align-items-start" >
+        <div className="row align-items-start brilliant-images-container" >
         {
-            brilliantImages.map((brilliantImage)=><img key={brilliantImage} className="col-12 col-lg-4" src={`assets/images/brilliant-${brilliantImage}.jpg`} />)
+            brilliantImages.map((brilliantImage)=><img className="col-xl-4 col-12 brilliant-image" key={brilliantImage} className="col-12 col-lg-4" src={`assets/images/brilliant-${brilliantImage}.jpg`} />)
         }
         
         </div>
--- day-21/src/components/Team.jsx
+++ day-21/src/components/Team.jsx
@@ -9,10 +9,10 @@ const Team = () => {
     <div className="padding-container my-5 common-container">
       <div className="container-fluid py-5">
         <div className="row gap-1 justify-content-between align-items-start">
-          <div className="col-12 col-lg-3">
+          <div className="col-12 col-xl-3">
             <SecondaryHeadline text="the zenith team" />
           </div>
-          <div className="col-12 col-lg-6">
+          <div className="col-12 col-xl-6">
             <DescriptionText text={description} />
           </div>
         </div>
