297075f1648c5ba16b39f75a7456e8d40e738388
react-learner <suhashsai1999@gmail.com>
2024-05-17 20:44:17 +0530

    footer responsiveness completed

--- day-21/src/components/Footer.jsx
+++ day-21/src/components/Footer.jsx
@@ -1,32 +1,40 @@
+import "../styles/Footer.css";
+
 const Footer = () => {
   return (
     <div className="padding-container my-5">
       <div className="container-fluid">
-      <div></div>
-        <div className="row mb-5 pb-5 ">
-          <div className="d-flex align-items-start justify-content-between ">
-            <p className="article-title">Investment approach</p>
-            <p className="article-title">Sustainability</p>
-            <p className="article-title">Team</p>
-            <p className="article-title">Login</p>
-          </div>
+        <div></div>
+        <div className="row my-5 py-5 footer-container ">
+          <p className="article-title col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 ">
+            Investment approach
+          </p>
+          <p className="article-title col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 ">
+            Sustainability
+          </p>
+          <p className="article-title col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 ">
+            Team
+          </p>
+          <p className="article-title col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12 ">
+            Login
+          </p>
         </div>
-        <div className="row mt-5 pt-5 ">
-          <div className="d-flex align-items-start text-start justify-content-between ">
-            <p className="article-title">
-              Zenith Venture Capital
-              <br />
-              Kungsgatan 3<br /> 111 43 Stockholm (SE)
-            </p>
-            <p className="navbar-brand">zenith</p>
-          </div>
+        <div className="row ">
+          <p className=" col-md-6 article-title">
+            Zenith Venture Capital
+            <br />
+            Kungsgatan 3<br /> 111 43 Stockholm (SE)
+          </p>
+          <p className=" col-md-6 navbar-brand">zenith</p>
         </div>
-        <div className="row my-5 pt-5" >
-        <div className="d-flex flex-row gap-4 align-items-start  " >
-        <p className="fw-medium" >Investor Relations</p>
-        <p className="fw-medium" >&copy; 2024 Zenith Venture Capital</p>
-        <p className="fw-medium" >Design & development by E&W Design Studio</p>
-        </div>
+        <div className="row my-5 py-5 footer-container ">
+          <p className="fw-medium col-md-4 ">Investor Relations</p>
+          <p className="fw-medium col-md-4 ">
+            &copy; 2024 Zenith Venture Capital
+          </p>
+          <p className="fw-medium col-md-4 ">
+            Design & development by E&W Design Studio
+          </p>
         </div>
       </div>
     </div>
