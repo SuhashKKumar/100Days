3bd49bc71cf44cf7fc05566e4a135a907a20ee16
suhashkkumar <suhashkkumar@gmail.com>
2024-05-23 14:20:36 +0000

    added few more nav-items

--- day-23/src/components/Navbar.jsx
+++ day-23/src/components/Navbar.jsx
@@ -29,24 +29,24 @@ const Navbar = () => {
           className="w-6 "
         />
         <p className="font-bold text-slate-600 text-lg">Microsoft</p>
-        <div className={`nav-items text-sm mx-4 gap-4 w-1/2 inset-0 bg-gray-200 mt-14 absolute -translate-x-${showMenu?4:96} md:w-fit md:translate-x-0 md:static md:mt-1 md:flex md:items-center md:bg-transparent`}>
-          <div className="nav-item my-4 mt-4 md:mt-0 md:my-0 w-fit hover:underline underline-offset-4 cursor-pointer">
+        <div className={`nav-items text-sm mx-4 md:gap-4 md:px-0 w-full inset-0 bg-gray-200 mt-14 absolute  ${showMenu? '-translate-x-4': '-translate-x-[-100rem]'} md:w-fit md:translate-x-0 md:static md:mt-1 md:flex md:items-center md:bg-transparent`}>
+          <div className="nav-item p-3 md:px-0 md:py-0 text-lg md:text-sm border-b-gray-300 border w-full  md:border-0  md:w-fit md:hover:underline underline-offset-4 cursor-pointer">
             Microsoft 365
           </div>
-          <div className="nav-item my-4 md:my-0 w-fit hover:underline underline-offset-4 cursor-pointer">
+          <div className="nav-item p-3 md:px-0 md:py-0 text-lg md:text-sm border-b-gray-300 border w-full  md:border-0 md:w-fit md:hover:underline underline-offset-4 cursor-pointer">
             Teams
           </div>
-          <div className="nav-item my-4 md:my-0 w-fit hover:underline underline-offset-4 cursor-pointer">
+          <div className="nav-item p-3 md:px-0 md:py-0 text-lg md:text-sm border-b-gray-300 border w-full  md:border-0 md:w-fit md:hover:underline underline-offset-4 cursor-pointer">
             Copilot
           </div>
-          <div className="nav-item my-4 md:my-0 w-fit hover:underline underline-offset-4 cursor-pointer">
+          <div className="nav-item p-3 md:px-0 md:py-0 text-lg md:text-sm border-b-gray-300 border w-full  md:border-0 md:w-fit md:hover:underline underline-offset-4 cursor-pointer">
             Windows
           </div>
 
           <select
             name="More"
             id="More"
-            className=" hidden md:inline-block nav-item my-2 md:my-0 w-fit hover:underline underline-offset-4 cursor-pointer lg:hidden bg-transparent "
+            className=" hidden md:inline-block my-2 md:my-0 md:w-fit md:hover:underline underline-offset-4 cursor-pointer lg:hidden bg-transparent "
           >
             <option value="" selected disabled hidden>
               More
@@ -62,23 +62,32 @@ const Navbar = () => {
             </option>
           </select>
 
-          <div className="nav-item w-fit hover:underline underline-offset-4 cursor-pointer block my-4 md:my-0 md:hidden lg:inline-block ">
+          <div className="nav-item p-3 md:px-0 md:py-0 text-lg md:text-sm border-b-gray-300 border w-full  md:border-0 md:w-fit md:hover:underline underline-offset-4 cursor-pointer block md:hidden lg:inline-block ">
             Surface
           </div>
-          <div className="nav-item w-fit hover:underline underline-offset-4 cursor-pointer block my-4 md:my-0 md:hidden lg:inline-block ">
+          <div className="nav-item p-3 md:px-0 md:py-0 text-lg md:text-sm border-b-gray-300 border w-full  md:border-0 md:w-fit md:hover:underline underline-offset-4 cursor-pointer block md:hidden lg:inline-block ">
             Xbox
           </div>
-          <div className="nav-item w-fit hover:underline underline-offset-4 cursor-pointer block my-4 md:my-0 md:hidden lg:inline-block ">
+          <div className="nav-item p-3 md:px-0 md:py-0 text-lg md:text-sm border-b-gray-300 border w-full  md:border-0 md:w-fit md:hover:underline underline-offset-4 cursor-pointer block md:hidden lg:inline-block ">
             Support
+          </div>
+          <div className="nav-item p-3 text-lg border-b-gray-300 border w-full cursor-pointer block md:hidden ">
+            Software
+          </div>
+          <div className="nav-item p-3 text-lg border-b-gray-300 border w-full cursor-pointer block md:hidden ">
+            PCs & Devices
+          </div>
+          <div className="nav-item p-3 text-lg border-b-gray-300 border w-full cursor-pointer block md:hidden ">
+            Entertainment
           </div>
         </div>
       </div>
       <div className="flex items-center gap-5 md:order-last mt-1 ">
-        <p className="hidden lg:inline-block text-sm hover:underline underline-offset-4 cursor-pointer">
+        <p className="hidden lg:inline-block text-sm md:hover:underline underline-offset-4 cursor-pointer">
           All Microsoft
         </p>
         <div className="flex item-center mt-1 xl:gap-1">
-          <p className="text-sm hidden xl:inline-block hover:underline underline-offset-4 cursor-pointer">
+          <p className="text-sm hidden xl:inline-block md:hover:underline underline-offset-4 cursor-pointer">
             Search
           </p>
           <img
@@ -88,13 +97,13 @@ const Navbar = () => {
           />
         </div>
         <div className="flex item-center mt-1 xl:gap-1">
-          <p className="text-sm hidden xl:inline-block hover:underline underline-offset-4 cursor-pointer">
+          <p className="text-sm hidden xl:inline-block md:hover:underline underline-offset-4 cursor-pointer">
             Cart
           </p>
           <img src="/assets/cart-icon.svg" alt="Cart" className="w-6 " />
         </div>
         <div className="flex item-center mt-1 xl:gap-1">
-          <p className="text-sm hidden xl:inline-block hover:underline underline-offset-4 cursor-pointer">
+          <p className="text-sm hidden xl:inline-block md:hover:underline underline-offset-4 cursor-pointer">
             Sign In
           </p>
           <img
