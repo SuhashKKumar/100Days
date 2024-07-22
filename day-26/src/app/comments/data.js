61409ba6739237a8fb96ba0cbb616fcdc18883f7
react-learner <suhashsai1999@gmail.com>
2024-07-22 10:08:25 +0530

    multiple query parameters handler for comments route

--- day-26/src/app/comments/data.js
+++ day-26/src/app/comments/data.js
@@ -1,18 +1,18 @@
 export const comments = [
   {
     id: 1,
-    text: "This is the first comment.",
+    text: "This is the first comment third",
   },
   {
     id: 2,
-    text: "This is the second comment.",
+    text: "This is the second comment fourth",
   },
   {
     id: 3,
-    text: "This is the third comment.",
+    text: "This is the third comment second",
   },
   {
     id: 4,
-    text: "This is the fourth comment.",
+    text: "This is the fourth comment first",
   },
 ];
--- day-26/src/app/comments/route.js
+++ day-26/src/app/comments/route.js
@@ -4,6 +4,7 @@ import { comments } from "./data";
 //   return Response.json(comments);
 // }
 
+// single query parameter handler
 export async function GET(request) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("query");
@@ -29,3 +30,23 @@ export async function POST(request) {
     status: 201,
   });
 }
+
+// // multiple query parameters handler
+// export async function GET(request) {
+//   const searchParams = await request.nextUrl.searchParams;
+//   const queryParams = ["first", "second"];
+//   let filterResult = [];
+//   const filtering = (item, filterList) => {
+//     return item
+//       ? filterList.filter((comment) =>
+//           comment.text.toLowerCase().includes(item.toLowerCase())
+//         )
+//       : filterList;
+//   };
+//   queryParams.forEach((queryItem) => {
+//     const query = searchParams.get(queryItem);
+//     if (queryParams[0] === query) filterResult = filtering(query, comments);
+//     else filterResult = filtering(query, filterResult);
+//   });
+//   return Response.json(filterResult);
+// }
