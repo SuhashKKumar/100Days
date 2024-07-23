c384e4345e6e040fb268761f29103a17dae6a4d2
react-learner <suhashsai1999@gmail.com>
2024-07-23 09:39:53 +0530

    middleware

---
+++ day-26/src/middleware.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+
+// // first approach, using custom matcher config
+// ===============================================
+// export function middleware(request){
+//     return NextResponse.redirect(new URL("/", request.url))
+// }
+
+// export const config={
+//     matcher:"/profile"
+// }
+
+// // second approach, using conditional approach
+// ===============================================
+// export function middleware(request) {
+//   if (request.nextUrl.pathname === "/profile")
+//     return NextResponse.redirect(new URL("/hello", request.url));
+// }
+
+// working with cookies in middleware
+// ===============================================
+export function middleware(request) {
+  const response = NextResponse.next();
+  const loginPreference = request.cookies.get("login");
+  if (!loginPreference) response.cookies.set("login", "admin");
+
+//   adding custom headers in middleware
+// =====================================
+response.headers.set("custom-headers","custom-value")
+
+  return response;
+}
