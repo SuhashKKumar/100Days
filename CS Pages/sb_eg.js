ab197817058e25ecb7641e04b658f6dd4201b36d
react-learner <suhashsai1999@gmail.com>
2025-04-08 10:15:28 +0530

    started canva project

--- CS Pages/sb_eg.js
+++ CS Pages/sb_eg.js
@@ -1,11 +1,10 @@
-function canada(instance, assetPath) {
+function getShoppingBagConfig(instance, assetPath) {
     window.personalizedMarketingData = window.personalizedMarketingData || {};
     window.personalizedMarketingData.components =
         window.personalizedMarketingData.components || {};
     //Shop Bag EN
     window.personalizedMarketingData.components[instance] = {
         instanceName: instance,
-        experimentRunning: true,
         name: "LayeredContentModule",
         type: "sitewide",
         useGreyLoadingEffect: false,
@@ -33,12 +32,12 @@ if (typeof gap.components !== "undefined" && gap.components == true) {
     var locale = window.__OPTLY_STATE__.shared.getStateValue("locale");
 
     if (locale === "en_CA") {
-        canada(
+        getShoppingBagConfig(
             "shopbag-ilp-en",
             "/Asset_Archive/ONWeb/content/0019/595/575/assets/220331_NS_N2827_ILP_CAPilotExp_OptInBonus_Site_ShoppingBag.svg"
         );
     } else if (locale === "fr_CA") {
-        canada(
+        getShoppingBagConfig(
             "shopbag-ilp-fr",
             "/Asset_Archive/ONWeb/content/0019/595/575/assets/220331_NS_N2827_ILP_CAPilotExp_OptInBonus_Site_ShoppingBag.svg"
         );
